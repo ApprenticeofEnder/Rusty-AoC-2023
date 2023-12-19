95be88405c05d64ca9c12757f89a306a01d5d59a
ApprenticeofEnder <perfknight@gmail.com>
2023-12-19 01:33:45 -0500

    finish day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use polars::{lazy::dsl::count, prelude::*};
+
+fn main() -> Result<(), PolarsError> {
+    let df = CsvReader::from_path("data/network_traffic.csv")
+        .unwrap()
+        .finish()?;
+
+    //Task 1: Count the Packets
+    let processed1 = df.clone().lazy().select([count()]).collect()?;
+
+    let res1 = processed1["count"].iter().next().unwrap();
+
+    println!("Number of packets in file: {}", res1);
+
+    //Task 2
+    let processed2 = df
+        .clone()
+        .lazy()
+        .group_by(["Source"])
+        .agg([count()])
+        .sort(
+            "count",
+            SortOptions {
+                descending: true,
+                nulls_last: true,
+                ..Default::default()
+            },
+        )
+        .limit(1)
+        .collect()?;
+    let res2 = processed2["Source"].iter().next().unwrap();
+
+    println!("Most frequent sender: {}", res2.get_str().unwrap());
+
+    //Task 3
+    let processed3 = df
+        .clone()
+        .lazy()
+        .group_by(["Protocol"])
+        .agg([count()])
+        .sort(
+            "count",
+            SortOptions {
+                descending: true,
+                nulls_last: true,
+                ..Default::default()
+            },
+        )
+        .limit(1)
+        .collect()?;
+
+    let res3 = processed3["Protocol"].iter().next().unwrap();
+
+    println!("Most frequent protocol: {}", res3.get_str().unwrap());
+    Ok(())
 }
