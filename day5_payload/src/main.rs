b3c467fbe82b89e589254355e8cd0e8da9390562
ApprenticeofEnder <perfknight@gmail.com>
2023-12-22 14:49:03 -0500

    formatting

--- day5_payload/src/main.rs
+++ day5_payload/src/main.rs
@@ -5,50 +5,44 @@ extern crate alloc;
 
 mod dos_tests;
 
-use day5_payload::{*, dos::file::File};
+use day5_payload::{dos::file::File, *};
 
 entry!(main);
 
 fn main() {
     let filename_src: &str = "TEST.TXT";
-
     let filename_out: &str = "TEST_2.TXT";
 
     let fix_bytes: [u8; 2] = [0x41, 0x43];
 
     println!("Opening: {}", filename_src);
-
     let src_file: File = File::open(filename_src).expect("Check file name and try again?");
 
     println!("Reading...");
-
     let mut buf = [0; 13000]; //Target file is a little over 12,700 bytes
-
     let bytes_read = src_file.read(&mut buf).unwrap();
-
     println!("Read {} bytes", bytes_read);
 
     src_file.close().unwrap();
-
     println!("Closed source file.");
 
     println!("Opening for write: {}", filename_out);
-
     let out_file: File = File::open(filename_out).expect("Error opening outfile.");
-    
+
     fix_bytes.iter().enumerate().for_each(|(index, byte)| {
         buf[index] = *byte;
     });
 
     println!("First two bytes of buffer: {} {}", buf[0], buf[1]);
-    println!("First two bytes of patch buffer: {} {}", fix_bytes[0], fix_bytes[1]);
-
+    println!(
+        "First two bytes of patch buffer: {} {}",
+        fix_bytes[0], fix_bytes[1]
+    );
     assert_eq!((buf[0], buf[1]), (fix_bytes[0], fix_bytes[1]));
 
     let bytes_written = out_file.write(&buf).unwrap();
     println!("Wrote {} bytes", bytes_written);
 
     out_file.close().unwrap();
-
     println!("Modification complete. Please confirm modifications manually.");
 }
