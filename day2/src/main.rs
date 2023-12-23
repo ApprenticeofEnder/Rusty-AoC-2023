192453bf9740528efa9c3c7cace046d94b009e9a
ApprenticeofEnder <perfknight@gmail.com>
2023-12-23 01:15:26 -0500

    offload 'most popular' calculations to separate function

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,11 +1,29 @@
 use polars::error::PolarsResult;
 use polars::{lazy::dsl::count, prelude::*};
 
+fn most_popular<'a>(column: &'a str, df: &'a DataFrame) -> PolarsResult<DataFrame> {
+    Ok(df
+        .clone()
+        .lazy()
+        .group_by([column])
+        .agg([count()])
+        .sort(
+            "count",
+            SortOptions {
+                descending: true,
+                nulls_last: true,
+                ..Default::default()
+            },
+        )
+        .limit(1)
+        .collect()?)
+}
+
 fn main() -> PolarsResult<()> {
-    let df = CsvReader::from_path("data/network_traffic.csv")?.finish()?;
+    let df: DataFrame = CsvReader::from_path("data/network_traffic.csv")?.finish()?;
 
     //Task 1: Count the Packets
-    let processed1 = df.clone().lazy().select([count()]).collect()?;
+    let processed1: DataFrame = df.clone().lazy().select([count()]).collect()?;
 
     if let Some(res1) = processed1["count"].iter().next() {
         println!("Number of packets in file: {}", res1);
@@ -13,22 +31,8 @@ fn main() -> PolarsResult<()> {
         println!("Something went wrong with task 1.");
     }
 
-    //Task 2
-    let processed2 = df
-        .clone()
-        .lazy()
-        .group_by(["Source"])
-        .agg([count()])
-        .sort(
-            "count",
-            SortOptions {
-                descending: true,
-                nulls_last: true,
-                ..Default::default()
-            },
-        )
-        .limit(1)
-        .collect()?;
+    //Task 2: IP Address with the most packets sent
+    let processed2: DataFrame = most_popular("Source", &df)?;
 
     if let Some(res2) = processed2["Source"].iter().next() {
         println!("Most frequent sender: {}", res2.get_str().unwrap());
@@ -36,22 +40,8 @@ fn main() -> PolarsResult<()> {
         println!("Something went wrong with task 2.");
     }
 
-    //Task 3
-    let processed3 = df
-        .clone()
-        .lazy()
-        .group_by(["Protocol"])
-        .agg([count()])
-        .sort(
-            "count",
-            SortOptions {
-                descending: true,
-                nulls_last: true,
-                ..Default::default()
-            },
-        )
-        .limit(1)
-        .collect()?;
+    //Task 3: Most Popular Protocol
+    let processed3: DataFrame = most_popular("Protocol", &df)?;
 
     if let Some(res3) = processed3["Protocol"].iter().next() {
         println!("Most frequent protocol: {}", res3.get_str().unwrap());
